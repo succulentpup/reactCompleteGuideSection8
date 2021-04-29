639df4d892c6700345caa0347a679b88d7af278e
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-04-29 16:21:30 +0100

    reset the form when its submitted. Input validation to age input is added.

--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,25 +1,35 @@
+import { useState} from "react";
 import Button from '../UI/Button';
 import Card from "../UI/Card";
 
 import classStyles from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [userName, setUserName] =useState('');
+  const [age, setAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (userName.trim().length === 0 || age.trim().length === 0) return;
+    if (+age.trim() < 1 || +age.trim() > 120) return;
+    setUserName('');
+    setAge('');
   }
+
+  const userNameOnChangeHandler = (event) => setUserName(event.target.value);
+  const ageOnChangeHandler = (event) => setAge(event.target.value);
 
   return (
     <Card className={classStyles.input}>
       <form action="">
         <label htmlFor="username">Username</label>
-        <input id='username' type="text"/>
-        <label htmlFor="age">Age</label>
-        <input id='age' type="number"/>
+        <input id='username' type="text" value={userName} onChange={userNameOnChangeHandler}/>
+        <label htmlFor="age">Age (Years)</label>
+        <input id='age' type="number" value={age} onChange={ageOnChangeHandler}/>
         <Button onClick={addUserHandler}>AddUser</Button>
       </form>
     </Card>
   )
-
 }
 
 export default AddUser;
