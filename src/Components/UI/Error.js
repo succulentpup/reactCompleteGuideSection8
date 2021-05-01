c89abd5cd4e710a617889f0948435dba3ba246d0
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-05-01 11:22:00 +0100

    added error component

---
+++ src/Components/UI/Error.js
@@ -0,0 +1,26 @@
+import Card from './Card';
+import Button from './Button';
+
+import styledClasses from './Error.module.css';
+
+const Error = (props) => {
+  return (
+    <div>
+      <div className={styledClasses.backdrop} onClick={props.onClick}>
+        <Card className={styledClasses.modal}>
+          <header className={styledClasses.header}>
+            <h2>{props.title}</h2>
+          </header>
+          <div className={styledClasses.content}>
+            <p>{props.message}</p>
+          </div>
+          <footer className={styledClasses.actions}>
+            <Button type='button' onClick={props.onClick}>Okey</Button>
+          </footer>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,17 +1,32 @@
-import { useState} from "react";
-import Button from '../UI/Button';
-import Card from "../UI/Card";
+import { useState} from 'react';
 
 import classStyles from './AddUser.module.css';
+
+import Button from '../UI/Button';
+import Card from '../UI/Card';
+import Error from '../UI/Error';
 
 const AddUser = (props) => {
   const [userName, setUserName] =useState('');
   const [age, setAge] = useState('');
+  const [error, setError] = useState(undefined);
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (userName.trim().length === 0 || age.trim().length === 0) return;
-    if (+age.trim() < 1 || +age.trim() > 120) return;
+    if (userName.trim().length === 0 || age.trim().length === 0) {
+      setError({
+        title: 'Empty input',
+        message: 'Name or Age should not be empty',
+      });
+      return;
+    }
+    if (+age.trim() < 1 || +age.trim() > 120) {
+      setError({
+        title: 'Invalid age',
+        message: 'Age should be > 0 & < 120',
+      })
+      return;
+    }
     props.appUsersHandler({ id: Math.random(), name: userName, age });
     setUserName('');
     setAge('');
@@ -20,16 +35,23 @@ const AddUser = (props) => {
   const userNameOnChangeHandler = (event) => setUserName(event.target.value);
   const ageOnChangeHandler = (event) => setAge(event.target.value);
 
+  const errorHandler = () => setError(undefined);
+
   return (
-    <Card className={classStyles.input}>
-      <form action="">
-        <label htmlFor="username">Username</label>
-        <input id='username' type="text" value={userName} onChange={userNameOnChangeHandler}/>
-        <label htmlFor="age">Age (Years)</label>
-        <input id='age' type="number" value={age} onChange={ageOnChangeHandler}/>
-        <Button onClick={addUserHandler}>AddUser</Button>
-      </form>
-    </Card>
+    <div>
+      {error && <Error title={error.title} message={error.message} onClick={errorHandler}/>}
+      <Card className={classStyles.input}>
+        <form action="">
+          <label htmlFor="username">Username</label>
+          <input id='username' type="text" value={userName}
+                 onChange={userNameOnChangeHandler}/>
+          <label htmlFor="age">Age (Years)</label>
+          <input id='age' type="number" value={age}
+                 onChange={ageOnChangeHandler}/>
+          <Button onClick={addUserHandler}>AddUser</Button>
+        </form>
+      </Card>
+    </div>
   )
 }
 
