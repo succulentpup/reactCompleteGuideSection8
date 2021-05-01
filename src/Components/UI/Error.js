b4b7bb208e9c2df6fd34e0c7cf6abb9180982398
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-05-01 12:03:51 +0100

    refactor: used createPortal to fix the overlays and backdrop positions relative to DOM

--- src/Components/UI/Error.js
+++ src/Components/UI/Error.js
@@ -1,25 +1,38 @@
 import {Fragment} from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card';
 import Button from './Button';
 
 import styledClasses from './Error.module.css';
 
+const BackDrop = (props) => {
+  return <div className={styledClasses.backdrop} onClick={props.onConfirm}/>;
+};
+
+const ModalOverlay = (props) => {
+  return  <Card className={styledClasses.modal}>
+    <header className={styledClasses.header}>
+      <h2>{props.title}</h2>
+    </header>
+    <div className={styledClasses.content}>
+      <p>{props.message}</p>
+    </div>
+    <footer className={styledClasses.actions}>
+      <Button type='button' onClick={props.onConfirm}>Okey</Button>
+    </footer>
+  </Card>
+};
+
 const Error = (props) => {
   return (
     <Fragment>
-      <div className={styledClasses.backdrop} onClick={props.onConfirm}>
-        <Card className={styledClasses.modal}>
-          <header className={styledClasses.header}>
-            <h2>{props.title}</h2>
-          </header>
-          <div className={styledClasses.content}>
-            <p>{props.message}</p>
-          </div>
-          <footer className={styledClasses.actions}>
-            <Button type='button' onClick={props.onConfirm}>Okey</Button>
-          </footer>
-        </Card>
-      </div>
+      {ReactDOM.createPortal(<BackDrop onConfirm={props.onConfirm}/>, document.getElementById('modal-root'))};
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          onConfirm={props.onConfirm}
+          title={props.title}
+          message={props.message}
+        />, document.getElementById('overlay-root'))};
     </Fragment>
   );
 }
